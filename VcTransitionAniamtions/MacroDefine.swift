81ed88d47e0fc37bb76b75180370a3b21bff578a
Apple <apple@hs08sv01-float.iscp-hs.telcordia.com>
2017-05-16 12:45:01 +0800

    add

---
+++ VcTransitionAniamtions/MacroDefine.swift
@@ -0,0 +1,11 @@
+//
+//  MacroDefine.swift
+//  VcTransitionAniamtions
+//
+//  Created by Apple on 2017/5/16.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+import UIKit
+
+let screen = UIScreen.main.bounds.size
